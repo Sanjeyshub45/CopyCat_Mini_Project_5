b68d6942e87b7830c4f2dcaf9ec8d5fce2afa353
Sanjey S <sanjudote45@gmail.com>
2025-01-20 19:16:45 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,20 +1,17 @@
-const noteForm = document.getElementById("noteForm");
-const noteInput = document.getElementById("noteInput");
-const notesList = document.getElementById("notesList");
+const  noteForm =  document.getElementById("noteForm");
+const noteInput =  document.getElementById("noteInput");
+const  notesList = document.getElementById("notesList");
 const notesCounter = document.getElementById("notesCounter");
-const clearAllButton = document.getElementById("clearAllButton");
-const addButton = document.getElementById("addButton");
-const saveChangesButton = document.getElementById("saveChangesButton");
-
+const   clearAllButton = document.getElementById("clearAllButton");
+const  addButton = document.getElementById("addButton");
+const saveChangesButton =    document.getElementById("saveChangesButton");
 const copyToast = document.getElementById("copyToast"); 
-
 let notes = JSON.parse(localStorage.getItem("notes")) || [];
 let editIndex = -1;
 renderNotes();
 
 noteForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
     const noteText = noteInput.value.trim();
     if (noteText === "") return;
 
@@ -28,12 +25,10 @@ noteForm.addEventListener("submit", (e) => {
        
         notes.push({ text: noteText, time: new Date().toLocaleString() });
     }
-
     noteInput.value = "";
     saveNotesToLocalStorage();
     renderNotes();
 });
-
 saveChangesButton.addEventListener("click", () => {
     if (editIndex > -1) {
         const noteText = noteInput.value.trim();
@@ -45,23 +40,21 @@ saveChangesButton.addEventListener("click", () => {
         noteInput.value = "";
         saveChangesButton.classList.add("hidden");
         addButton.classList.remove("hidden");
-
-        saveNotesToLocalStorage();
+ saveNotesToLocalStorage();
         renderNotes();
     }
 });
-
 function renderNotes() {
     notesList.innerHTML = "";
     notes.forEach((note, index) => {
-        const noteItem = document.createElement("li");
+    const noteItem = document.createElement("li");
         noteItem.innerHTML = `
             <span class="note-number">${index + 1}.</span>
-            <div class="note-content">
+        <div class="note-content">
                 <p>${note.text}</p>
                 <span class="note-time">${note.time}</span>
             </div>
-            <button class="edit-button"><i class="fa-solid fa-pen"></i></button>
+          <button class="edit-button"><i class="fa-solid fa-pen"></i></button>
             <button class="delete-button"><i class="fa-solid fa-trash"></i></button>
             <button class="copy-button"><i class="fa-solid fa-copy"></i></button>
         `;
@@ -70,45 +63,39 @@ function renderNotes() {
         const deleteButton = noteItem.querySelector(".delete-button");
         const copyButton = noteItem.querySelector(".copy-button");
         editButton.addEventListener("click", () => {
-            noteInput.value = note.text;
+     noteInput.value = note.text;
             editIndex = index;
-            saveChangesButton.classList.remove("hidden");
+         saveChangesButton.classList.remove("hidden");
             addButton.classList.add("hidden");
         });
         deleteButton.addEventListener("click", () => {
             noteItem.classList.add("deleted"); 
-            setTimeout(() => {
+         setTimeout(() => {
                 notes.splice(index, 1); 
                 saveNotesToLocalStorage(); 
                 renderNotes(); 
             }, 500); 
         });
-
-        
-        copyButton.addEventListener("click", () => {
-            navigator.clipboard.writeText(note.text)
+ copyButton.addEventListener("click", () => {
+        navigator.clipboard.writeText(note.text)
                 .then(() => {
                     showCopyToast(); 
                 })
                 .catch((err) => {
                     console.error("Error copying text: ", err);
-                });
+                }
+                      );
         });
-
-        notesList.appendChild(noteItem);
+notesList.appendChild(noteItem);
     });
-
-    notesCounter.textContent = `Loaded Ideas: ${notes.length}`;
+  notesCounter.textContent = `Loaded Ideas: ${notes.length}`;
 }
 
-clearAllButton.addEventListener("click", () => {
+   clearAllButton.addEventListener("click", () => {
     notes = [];
-    saveNotesToLocalStorage();
+       saveNotesToLocalStorage();
     renderNotes();
-});
-
-
-function showCopyToast() {
+});function showCopyToast() {
   const copyToast = document.getElementById("copyToast");
   copyToast.classList.add("show");
 
